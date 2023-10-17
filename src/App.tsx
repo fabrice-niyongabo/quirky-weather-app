e4123b32e18aaf24f2ab1ad7d6771fa4b9912fcc
Fabrice Niyongabo <ninga.fab@gmail.com>
2023-10-17 18:00:59 +0200

    created home route

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
-import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./views/home";
 
 function App() {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
