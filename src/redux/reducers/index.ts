be36489cb17beeb8c0deb385f1a8782e60da9be1
Fabrice Niyongabo <ninga.fab@gmail.com>
2023-10-19 17:47:13 +0200

    backend init

--- src/redux/reducers/index.ts
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from "redux";
-import appReducer from "./app";
-const rootReducer = combineReducers({
-  appReducer,
-});
-
-export default rootReducer;
-
-export type RootState = ReturnType<typeof rootReducer>;
