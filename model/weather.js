e2904c5396055cba49a76b572c2aba80bd523da0
Fabrice Niyongabo <ninga.fab@gmail.com>
2023-10-19 18:37:26 +0200

    weather model

---
+++ model/weather.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const weatherSchema = new mongoose.Schema({
+  userId: { type: String, required: true },
+  rwandanCityName: { type: String, required: true },
+  swedenCityName: { type: String, required: true },
+  rwandanCityWeather: { type: Object, required: true },
+  swedenCityWeather: { type: Object, required: true },
+
+  //   temperature: { type: String, required: true },
+  //   windSpeed: { type: String, required: true },
+  //   icon: { type: String, required: true },
+  //   humidity: { type: String, required: true },
+  //   pressure: { type: String, required: true },
+  //   description: { type: String, required: true },
+  createdAt: { type: String, default: new Date() },
+});
+
+module.exports = mongoose.model("weather", weatherSchema);
